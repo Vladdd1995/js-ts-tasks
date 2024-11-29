d3ed4d5469449266f215103781ae469ccfd8514d
Vladdd1995 <yafimenka.uladzislau@student.ehu.lt>
2024-11-29 10:37:31 +0000

    умма элементов от 0-го индекса до текущего

--- src/partialSum.js
+++ src/partialSum.js
@@ -15,8 +15,8 @@ module.exports.partialSum = function partialSum(arr) {
   let result = [];
   let sum = 0;
   for (let i = 0; i < arr.length; i++) {
-    sum += arr[i];
-    result.push(sum);
+  sum += arr[i];
+  result.push(sum);
   }
   return result;
 };
